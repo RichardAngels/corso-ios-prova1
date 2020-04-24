dc22fd1948b398b03ed074d67c3626f10f7e4137
RichardAngels <r_angioli@icloud.com>
2020-04-24 13:24:06 +0200

    Initial Commit

---
+++ prova1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  prova1
+//
+//  Created by Riccardo Angioli on 24/04/2020.
+//  Copyright © 2020 Riccardo Angioli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
